d253d5883ce6c10daf7b8a62b752577ec13ac4b2
Carolina Rita Beirigo de Souza <Carol@iMac-de-Gabriel.local>
2024-02-15 13:02:32 -0300

    atualização do arquivo com base na aula sobre custom hook

--- 3_hooks/src/App.jsx
+++ 3_hooks/src/App.jsx
@@ -1,12 +1,10 @@
-
-import ComponenteFilho from "../components/ComponenteFilho";
-import Contador from "../components/Contador";
-import ExemploUseEffect from "../components/ExemploUseEffect";
-import Timer from "../components/Timer";
-import ValorDoContexto from "../components/ValorDoContexto";
-import { MeuContextoProvider } from "../contexts/MeuContexto";
-
-
+import ComponenteFilho from "./components/ComponenteFilho";
+import Contador from "./components/Contador";
+import ExemploUseEffect from "./components/ExemploUseEffect";
+import Timer from "./components/Timer";
+import ValorDoContexto from "./components/ValorDoContexto";
+import { MeuContextoProvider } from "./contexts/MeuContexto";
+import DisplayWindowSize from "./components/DisplayWindowSize";
 
 import "./App.css";
 
@@ -20,14 +18,16 @@ function App() {
       {/* 19 - useContext */}
       {/* aplicações de pequeno e médio porte, que precisam transferir o estado entre componentes */}
       <MeuContextoProvider>
-        <ComponenteFilho/>
-        <ValorDoContexto/>
+        <ComponenteFilho />
+        <ValorDoContexto />
       </MeuContextoProvider>
 
       {/* 20 - useReducer */}
       {/* estados mais complexos */}
-      <Contador/>
-     
+      {/* <Contador/> */}
+
+      {/* 21 - Custom Hooks */}
+      <DisplayWindowSize />
     </>
   );
 }
