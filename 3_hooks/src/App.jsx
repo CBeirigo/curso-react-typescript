707bf6d151e438f2add0fcf0b858507fb916f9f9
Carolina Rita Beirigo de Souza <Carol@iMac-de-Gabriel.local>
2024-02-21 13:21:24 -0300

    término das aulas teoricas da seção 4

--- 3_hooks/src/App.jsx
+++ 3_hooks/src/App.jsx
@@ -7,6 +7,10 @@ import { MeuContextoProvider } from "./contexts/MeuContexto";
 import DisplayWindowSize from "./components/DisplayWindowSize";
 
 import "./App.css";
+import Container from "./components/Container";
+import PerfilDeUsuario from "./components/PerfilDeUsuario";
+import CalculoPesado from "./components/CalculoPesado";
+import ContadorCallback from "./components/ContadorCallback";
 
 function App() {
   return (
@@ -28,6 +32,23 @@ function App() {
 
       {/* 21 - Custom Hooks */}
       <DisplayWindowSize />
+
+    {/* 22 - Slots e children props */}
+    <Container>
+      <h1>Título da seção</h1>
+      <p>Este é o meu subtítulo</p>
+      <Contador/>
+    </Container>
+
+    {/* 23 - Sincronizar o estado com props */}
+    {/* prop => componente => chamada de API => resulta em um dado */}
+    <PerfilDeUsuario usuarioId={1}/>
+    <PerfilDeUsuario usuarioId={2}/>
+
+    {/* 24 - useMemo e useCallback */}
+    <CalculoPesado numero={5}/>
+    <ContadorCallback/>
+
     </>
   );
 }
---
+++ 3_hooks/src/components/CalculoPesado.jsx
@@ -0,0 +1,15 @@
+import { useState, useMemo } from "react";
+
+const CalculoPesado = ({numero}) => {
+  const resultadoCalculoPesado = useMemo(() => {
+    return operacaoPesada(numero);
+  },[numero]);
+  return <div>Resultado: {resultadoCalculoPesado}</div>;
+};
+
+const operacaoPesada = (num) => {
+  console.log("Realizando operação pesada");
+  return num * 1000;
+};
+
+export default CalculoPesado;
