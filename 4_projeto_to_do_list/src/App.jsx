368ed09657eb088a39f836b4647ae67092019459
Carolina Rita Beirigo de Souza <Carol@iMac-de-Gabriel.local>
2024-02-21 20:16:32 -0300

    término da seção 4 e criação do projeto to do list

---
+++ 4_projeto_to_do_list/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import TaskInput from "./components/TaskInput";
+import TaskList from "./components/TaskList";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (task) => {
+    // id, text, done
+
+    setTasks([...tasks, { id: Date.now(), text: task, done: false }]);
+
+    // localStorage
+  };
+
+  const deleteTask = (taskId) => {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  }
+  return (
+    <div>
+      <h1>Lista de Tarefas</h1>
+      <TaskInput onAddTask={addTask} />
+      <TaskList tasks={tasks} onDeleteTask={deleteTask}/>
+    </div>
+  );
+}
+
+export default App;
