929036091f4e103e0a4f659aa3cd12b94bb89f10
Carolina Rita Beirigo de Souza <Carol@iMac-de-Gabriel.local>
2024-02-22 17:46:24 -0300

    Conclusão do projeto to do list

--- 4_projeto_to_do_list/src/App.jsx
+++ 4_projeto_to_do_list/src/App.jsx
@@ -1,9 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TaskInput from "./components/TaskInput";
 import TaskList from "./components/TaskList";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || []
+  );
+
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks)), [tasks];
+  });
 
   const addTask = (task) => {
     // id, text, done
@@ -13,15 +20,33 @@ function App() {
     // localStorage
   };
 
+
   const deleteTask = (taskId) => {
     setTasks(tasks.filter((task) => task.id !== taskId));
-  }
+  };
+
+  const toggleTaskDone = (taskId) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === taskId ? { ...task, done: !task.done } : task
+      )
+    );
+  };
+
   return (
     <div>
       <h1>Lista de Tarefas</h1>
-      <TaskInput onAddTask={addTask} />
-      <TaskList tasks={tasks} onDeleteTask={deleteTask}/>
+      
+      <TaskInput onAddTask={addTask} onToggleTask={toggleTaskDone} />
+      
+      <TaskList
+        tasks={tasks}
+        onDeleteTask={deleteTask}
+        onToggleTaskDone={toggleTaskDone}
+      />
+      
     </div>
+    
   );
 }
 
