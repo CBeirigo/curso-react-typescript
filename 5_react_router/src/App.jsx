f6c1f6cefee60975adcfea75a138865d8351f283
Carolina Rita Beirigo de Souza <Carol@iMac-de-Gabriel.local>
2024-02-26 17:50:34 -0300

    Finalização do conteúdo da seção 6 - react router dom

---
+++ 5_react_router/src/App.jsx
@@ -0,0 +1,83 @@
+import "./App.css";
+
+import { Routes, Route, Link, NavLink } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
+import Profile from "./components/Profile";
+import Settings from "./components/Settings";
+import TaskDetails from "./components/TaskDetails";
+import NotFound from "./components/NotFound";
+
+function App() {
+  return (
+    <>
+      {/* 1 - Setup e configuração inicial */}
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Início</Link>
+          </li>
+          <li>
+            <Link to="/about">Sobre</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contato</Link>
+          </li>
+
+          {/* 2 - NavLink */}
+          <li>
+            <NavLink
+              className={({ isActive }) => (isActive ? "active-link" : "")}
+              to="/contact"
+            >
+              Contato
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/login">Login</NavLink>
+          </li>
+
+          {/* 5 - Rotas aninhadas */}
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+
+        {/* 4 - Rotas dinâmicas */}
+        <div>
+          <h2>Tarefas</h2>
+          <Link to="/tasks/1">Tarefa 1</Link>
+          <Link to="/tasks/2">Tarefa 2</Link>
+          <Link to="/tasks/3">Tarefa 3</Link>
+        </div>
+
+        {/* 6 - rota 404 */}
+        <div>
+          <h2>Itens</h2>
+          <Link to="/items/1">Item 1</Link>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        {/* 3 - useNavigate */}
+        <Route path="/login" element={<Login />} />
+        {/* 4 - Rotas dinâmicas */}
+        <Route path="/tasks/:taskId" element={<TaskDetails/>}/>
+        {/* 5 - Rotas aninhadas */}
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="profile" element={<Profile />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
+        {/* 6 - 404 */}
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ 5_react_router/src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+        <h1>Entre em contato conosco</h1>
+        <p>(34) 99875-9090</p>
+    </div>
+  )
+}
+
+export default Contact
