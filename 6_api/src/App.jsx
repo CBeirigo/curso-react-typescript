ded5388221dc0b3ed579a9ec5d8606581dec81a6
Carolina Beirigo <cs16572@gmail.com.com>
2024-03-07 12:29:53 -0300

    Aulas sobre tratamento de erros e custom hook com requisição para API

--- 6_api/src/App.jsx
+++ 6_api/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import FetchPosts from "./components/FetchPosts";
 import AxiosPosts from "./components/AxiosPosts";
 import PostManager from "./components/PostManager";
+import PostLoader from "./components/PostLoader";
+import PostViewer from "./components/PostViewer";
 
 function App() {
   return (
@@ -18,12 +20,29 @@ function App() {
         <Link to="/posts">Gerenciar posts</Link>
       </div>
 
+      {/* 3 - Tratamentos de erros */}
+      <div>
+        <Link to="/post/1">Carregar Post 1</Link>
+      </div>
+      <div>
+        <Link to="/post/999">Carregar Post 999</Link>
+      </div>
+      {/* 4 - Custom hook com API */}
+      <div>
+        <Link to="/post/view/2">Carregar Post 2</Link>
+      </div>
+      
+
       <Routes>
         {/* 1- GET com Fetch e Axios */}
         <Route path="/axios-posts" element={<AxiosPosts />} />
         <Route path="/fetch-posts" element={<FetchPosts />} />
         {/* 2 - Continuando requisições - POST e UPDATE/PUT */}
-        <Route path="/posts" element={<PostManager/>}/>
+        <Route path="/posts" element={<PostManager />} />
+        {/* 3 - Tratamentos de erros */}
+        <Route path="/post/:postId" element={<PostLoader/>}/>
+        {/* 4 - Custom hook com API */}
+        <Route path="/post/view/:postId" element={<PostViewer/>}/>
       </Routes>
     </BrowserRouter>
   );
